3068a9f7f26c7fbc84704a1ed6ffaef301828890
Juloos <juloos.sfr@gmail.com>
2022-12-15 16:29:21 +0100

    refactor v3

--- ELF.c
+++ ELF.c
@@ -57,27 +57,34 @@ Elf32_Shdr * create_ELFTableSection(int nbSection) {
 }
 
 void ReadELFTabSection(FILE* file, Elf32_Shdr* tableSection, int nbSection) {
-    printf("Section Header Table\n")
     for (int i = 0; i < nbSection; i++) {
-        printf("\nSection %d\n    ", i);
         if (!fread(&tableSection[i].sh_name, sizeof(uint32_t), 1, file))
             perror("Read error\n");
+
         if (!fread(&tableSection[i].sh_type, sizeof(uint32_t), 1, file))
             perror("Read error\n");
+
         if (!fread(&tableSection[i].sh_flags, sizeof(uint32_t), 1, file))
             perror("Read error\n");
+
         if (!fread(&tableSection[i].sh_addr, sizeof(Elf32_Addr), 1, file))
             perror("Read error\n");
+
         if (!fread(&tableSection[i].sh_offset, sizeof(Elf32_Off), 1, file))
             perror("Read error\n");
+
         if (!fread(&tableSection[i].sh_size, sizeof(uint32_t), 1, file))
             perror("Read error\n");
+
         if (!fread(&tableSection[i].sh_link, sizeof(uint32_t), 1, file))
             perror("Read error\n");
+
         if (!fread(&tableSection[i].sh_info, sizeof(uint32_t), 1, file))
             perror("Read error\n");
+
         if (!fread(&tableSection[i].sh_addralign, sizeof(uint32_t), 1, file))
             perror("Read error\n");
+
         if (!fread(&tableSection[i].sh_entsize, sizeof(uint32_t), 1, file))
             perror("Read error\n");
     }
