0a2381ae51a9d96f25aac1818fc4f73d07462bef
BossyRomain <romain.bossy.01@gmail.com>
2022-12-15 11:10:00 +0100

    Ajout des définitions des fonctions pour lire et afficher le contenu d'un fichier ELF

---
+++ ELF.h
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define EI_NIDENT 16
+
+/**ReadELFFile
+ * Paramètres :
+ * - un pointeur sur un fichier ELF
+ * Résultat : lit et affiche sur la sortie standard le contenu du fichier ELF en paramètre
+*/
+void ReadELFFile(FILE* file);
+
+/**ReadELFHeader
+ * Paramètres :
+ * - un pointeur sur un fichier ELF
+ * Résultat : lit et affiche sur la sortie standard l'en tête du fichier ELF en paramètre
+*/
+void ReadELFHeader(FILE* file);
+
+/**ReadELFTableSection
+ * Paramètre : 
+ * - un pointeur sur un fichier ELF
+ * Résultat : lit et affiche sur la sortie standard la table des sections du fichier ELF en paramètre
+*/
+void ReadELFTableSection(FILE* file);
+
+/**ReadELFSection
+ * Paramètres :
+ * - un pointeur sur un fichier ELF
+ * - le numéro de la section à afficher
+ * Résultat : lit et affiche sur la sortie standard la section numéro numSection dans le fichier ELF en paramètre
+*/
+void ReadELFSection(FILE* file, int numSection);
+
+/**ReadELFSection
+ * Paramètres :
+ * - un pointeur sur un fichier ELF
+ * - le numéro de la section à afficher
+ * Résultat : lit et affiche sur la sortie standard la section nomSection dans le fichier ELF en paramètre
+*/
+void ReadELFSection(FILE* file, char* nomSection);
+
+/**ReadELFTableSymbols
+ * Paramètres :
+ * - un pointeur sur un fichier ELF
+ * Résultat : lit et affiche sur la sortie standard la table des symboles du fichier ELF en paramètre
+*/
+void ReadELFTableSymbols(FILE* file);
