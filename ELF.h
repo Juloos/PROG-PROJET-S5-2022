3169643ad2d897abd90447181abc456e1d0da21e
Juloos <juloos.sfr@gmail.com>
2022-12-16 10:44:26 +0100

    Affichage étape 2 + refactoring de fonctions

--- ELF.h
+++ ELF.h
@@ -6,50 +6,57 @@
  * Paramètres :
  * - un pointeur sur un fichier ELF
  * Résultat : lit et affiche sur la sortie standard le contenu du fichier ELF en paramètre
-*/
-void ReadELFFile(FILE* file);
+ */
+void ReadELFFile(FILE *file);
 
 /**ReadELFHeader
  * Paramètres :
  * - un pointeur sur un fichier ELF
  * Résultat : lit et affiche sur la sortie standard l'en tête du fichier ELF en paramètre
-*/
-void ReadELFHeader(FILE* file, Elf32_Ehdr* header);
-
+ */
+void ReadELFHeader(FILE *file, Elf32_Ehdr *ehdr);
 
 /**create_ELFTableSection
  * Paramètres :
  * - un entier représentant le nombre de sections
  * Résultat : retourne un pointeur sur un tableau de sections initialiser
-*/
+ */
 Elf32_Shdr * create_ELFTableSection(int nbSection);
 
 /**ReadELFTableSection
  * Paramètre : 
  * - un pointeur sur un fichier ELF
  * Résultat : lit et affiche sur la sortie standard la table des sections du fichier ELF en paramètre
-*/
-void ReadELFTableSection(FILE* file, Elf32_Shdr *tableSection, int nbSection, int offset);
+ */
+void ReadELFTableSection(FILE *file, Elf32_Shdr *shdrTable, int nbSection, int offset);
 
 /**ReadELFSection
  * Paramètres :
  * - un pointeur sur un fichier ELF
  * - le numéro de la section à afficher
  * Résultat : lit et affiche sur la sortie standard la section numéro numSection dans le fichier ELF en paramètre
-*/
-void ReadELFSectionNum(FILE* file, int numSection);
+ */
+void ReadELFSectionNum(FILE *file, int numSection);
 
 /**ReadELFSection
  * Paramètres :
  * - un pointeur sur un fichier ELF
  * - le numéro de la section à afficher
  * Résultat : lit et affiche sur la sortie standard la section nomSection dans le fichier ELF en paramètre
-*/
-void ReadELFSectionNom(FILE* file, char* nomSection);
+ */
+void ReadELFSectionNom(FILE *file, char *nomSection);
 
 /**ReadELFTableSymbols
  * Paramètres :
  * - un pointeur sur un fichier ELF
  * Résultat : lit et affiche sur la sortie standard la table des symboles du fichier ELF en paramètre
-*/
-void ReadELFTableSymbols(FILE* file);
+ */
+void ReadELFTableSymbols(FILE *file);
+
+/**PrintELFTableSection
+ * Paramètres :
+ * - un header ELF
+ * - un tableau des headers de section
+ * Résultat : affiche sur la sortie standard le contenu du tableau de headers
+ */
+void PrintELFTableSection(FILE *file, Elf32_Ehdr ehdr, Elf32_Shdr *shdrTable);
--- readELF.c
+++ readELF.c
@@ -1,53 +1,6 @@
 #include "ELF.h"
 #include <stdio.h>
 
-char * getSectionName(FILE * file, Elf32_Shdr * tableSection, int numSection, int offset) {
-    char * name = (char *) calloc(sizeof(char), tableSection[numSection].sh_size);
-    fseek(file, offset + tableSection[numSection].sh_name, SEEK_SET);
-    if (!fread(name, sizeof(char), tableSection[numSection].sh_size, file))
-        perror("Read error\n");
-    return name;
-}
-
-char * getSectionType(Elf32_Shdr * tableSection, int numSection) {
-    switch (tableSection[numSection].sh_type) {
-        case SHT_NULL:
-            return "NULL";
-        case SHT_PROGBITS:
-            return "PROGBITS";
-        case SHT_SYMTAB:
-            return "SYMTAB";
-        case SHT_STRTAB:
-            return "STRTAB";
-        case SHT_RELA:
-            return "RELA";
-        case SHT_HASH:
-            return "HASH";
-        case SHT_DYNAMIC:
-            return "DYNAMIC";
-        case SHT_NOTE:
-            return "NOTE";
-        case SHT_NOBITS:
-            return "NOBITS";
-        case SHT_REL:
-            return "REL";
-        case SHT_SHLIB:
-            return "SHLIB";
-        case SHT_DYNSYM:
-            return "DYNSYM";
-        case SHT_LOPROC:
-            return "LOPROC";
-        case SHT_HIPROC:
-            return "HIPROC";
-        case SHT_LOUSER:
-            return "LOUSER";
-        case SHT_HIUSER:
-            return "HIUSER";
-        default:
-    }
-    return "UNKNOWN";
-}
-
 int main(int argc, char* argv[]) {
     for(int i = 1; i < argc; i++) {
         FILE* file = fopen(argv[i], "r");
@@ -57,21 +10,7 @@ int main(int argc, char* argv[]) {
 
         Elf32_Shdr *sectionTable = create_ELFTableSection(header.e_shnum);
         ReadELFTableSection(file, sectionTable, header.e_shnum, header.e_shoff);
-
-        for (int i = 0; i < header.e_shnum; i++) {
-            printf("[%2d] %s %s %x %x %x %x %x %x %x %x\n", i,
-                   getSectionName(file, sectionTable, i, sectionTable[header.e_shstrndx].sh_offset),
-                   getSectionType(sectionTable, i),
-                   sectionTable[i].sh_flags,
-                   sectionTable[i].sh_addr,
-                   sectionTable[i].sh_offset,
-                   sectionTable[i].sh_size,
-                   sectionTable[i].sh_link,
-                   sectionTable[i].sh_info,
-                   sectionTable[i].sh_addralign,
-                   sectionTable[i].sh_entsize
-            );
-        }
+        PrintELFTableSection(file, header, sectionTable);
     }
     return 0;
 }
