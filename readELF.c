9fd043b1c148418945a3e043a4112a0faf4bf2c4
BossyRomain <romain.bossy.01@gmail.com>
2023-01-05 10:03:39 +0100

    Modification du programme pour afficher le contenu d'un ou de plusieurs fichiers ELF.
    Le programme permet maintenant d'afficher une partie précise du fichier ou alors tout le contenu du fichier.

--- readELF.c
+++ readELF.c
@@ -1,32 +1,76 @@
 #include "ELF.h"
 #include <stdio.h>
 
+#define PRINT_ALL 0
+#define PRINT_HEADER 1
+#define PRINT_SECTION_TABLE 2
+#define PRINT_SECTION 3
+#define PRINT_SYMBOL_TABLE 4
+#define PRINT_RELOCATION_TABLE 5
+
 int main(int argc, char* argv[]) {
-    for(int i = 1; i < argc; i++) {
+    // Si le premier arguments n'est pas un nombre alors on affiche tout les contenus des fichiers en paramaètres
+    int typeAffichage = atoi(argv[1]);
+
+    // Le nom ou le numéro de la section que l'on veut afficher si on veut afficher une section en particulier
+    char* section = argv[2];
+
+    // Pour savoir à partir de quel arguments commmence la liste des fichiers à afficher
+    int firstFile = 1;
+    if(typeAffichage) {
+        firstFile = 2;
+    }
+    // Si le 2ième argument n'est pas un fichier
+    FILE* file = fopen(argv[2], "r");
+    if(file == NULL) {
+        firstFile = 3;
+    }
+
+    for(int i = firstFile; i < argc; i++) {
         FILE* file = fopen(argv[i], "r");
         if (file == NULL) {
             fprintf(stderr, "Error opening file\n");
             exit(EXIT_FAILURE);
         }
 
+
         Elf32_Ehdr header;
         ReadELFHeader(file, &header);
-//        PrintELFHeader(header);
+        if(typeAffichage == PRINT_ALL || typeAffichage == PRINT_HEADER) {
+            PrintELFHeader(header);
+        }
 
         Elf32_Shdr *sectionTable = create_ELFTableSections(header);
         ReadELFTableSections(file, header, sectionTable);
-//        PrintELFTableSections(file, header, sectionTable);
-
-        for (int i = 0; i < header.e_shnum; i++) {
-            PrintELFSectionNum(file, header, sectionTable, i);
-            printf("\n");
+        if(typeAffichage == PRINT_ALL || typeAffichage == PRINT_SECTION_TABLE) {
+            PrintELFTableSections(file, header, sectionTable);
         }
-//        PrintELFSectionNom(file, header, sectionTable, ".text");
+
+        if(typeAffichage == PRINT_ALL) {
+            for (int i = 0; i < header.e_shnum; i++) {
+                PrintELFSectionNum(file, header, sectionTable, i);
+                printf("\n");
+            }
+        } else if(typeAffichage == PRINT_SECTION) {
+            int numSection = atoi(section);
+            // Affichage en fonction du nom
+            if(!numSection) {
+                PrintELFSectionNom(file, header, sectionTable, section);
+            }
+            // Affichage en fonction du numéro
+            else {
+                PrintELFSectionNum(file, header, sectionTable, numSection);
+            }
+        }
 
         Elf32_Shdr sh_symtab = sectionTable[sectionName2Index(".symtab", file, header, sectionTable)];
         Elf32_Sym *symbolTable = create_ELFTableSymbols(sh_symtab);
         ReadELFTableSymbols(file, symbolTable, sh_symtab);
-//        PrintELFTableSymbols(file, header, sectionTable, symbolTable);
+        if(typeAffichage == PRINT_ALL || typeAffichage == PRINT_SYMBOL_TABLE) {
+            PrintELFTableSymbols(file, header, sectionTable, symbolTable);
+        }
+
+        printf("\n");
 
         free(sectionTable);
         free(symbolTable);
