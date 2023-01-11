82cfb08d3e519bdb12f91f852d8f61f1d79ed07b
4ppolyon <78137075+4ppolyon@users.noreply.github.com>
2023-01-11 11:38:58 +0100

    Automatisation test 2

--- compile_tests.sh
+++ compile_tests.sh
@@ -4,8 +4,15 @@ for file in test*.s; do
     arm-none-eabi-gcc -mbig-endian -c "$file" -o "../$filename.o"
 done
 cd ../..
-for file1 in ./Examples_fusion/file*.o; do
-    for file2 in ./Examples_fusion/file*.o; do
+for file1 in ./Examples_fusion/file?.o; do
+    for file2 in ./Examples_fusion/file?.o; do
+        if [ "$file1" != "$file2" ]; then
+            ./oracle "$file1" "$file2"
+        fi
+    done
+done
+for file1 in ./Examples_fusion/exemple?.o; do
+    for file2 in ./Examples_fusion/exemple?.o; do
         if [ "$file1" != "$file2" ]; then
             ./oracle "$file1" "$file2"
         fi
